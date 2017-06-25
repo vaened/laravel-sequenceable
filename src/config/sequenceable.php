70c80e22e429cb3074f853d13d8481276b00befb
enea dhack <enea.so@live.com>
2017-06-24 23:52:52 -0500

    Comment the keys in the configuration file and add the autofill option

--- src/config/sequenceable.php
+++ src/config/sequenceable.php
@@ -6,5 +6,29 @@
  */
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sequence Model
+    |--------------------------------------------------------------------------
+    |
+    | This key defines the base sequence model that will be used to generate the
+    | autoincrementable values, you can modify this key and define your own
+    | sequence model as long as you implement the Sequenceable interface
+    |
+    */
    'model' => \Enea\Sequenceable\Model\Sequence::class,
+
+    /*
+     |---------------------------------------------------------------------------
+     | Auto filling
+     |---------------------------------------------------------------------------
+     | This option allows you to automatically fill the fields in your database, as specified in the model configuration.
+     | If you want to customize the value with which to autofill, you must change this key by replacing the
+     | value with the character you want, by default, if it is true, it will autocomplete to zero
+     |
+     */
+    'autofilling' => false,
+
+
 ];
