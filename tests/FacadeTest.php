14c654a37621646f842413a2436b706d45a05c51
enea dhack <enea.so@live.com>
2017-06-25 18:07:05 -0500

    Add test for facade

---
+++ tests/FacadeTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by enea dhack - 25/06/17 12:50 PM
+ */
+
+namespace Enea\Tests;
+
+use Enea\Sequenceable\Facades\Succession;
+use Enea\Tests\Models\DynamicCodeSequenceConfiguration;
+
+class FacadeTest extends DataBaseTestCase
+{
+
+    function test_the_sequences_of_a_model_are_obtained()
+    {
+
+        $document = new DynamicCodeSequenceConfiguration([ 'type' => 'tk' ]);
+        $document->save();
+        $this->assertDatabaseHas('documents', [ 'number' => 1 ]);
+
+        $document = new DynamicCodeSequenceConfiguration([ 'type' => 'iv' ]);
+        $document->save();
+        $this->assertDatabaseHas('documents', [ 'number' => 1 ]);
+
+        $collect = Succession::to( DynamicCodeSequenceConfiguration::class );
+
+        $ticket = $collect->first( )->toArray();
+
+        $this->assertSame($ticket['sequence'], 1);
+        $this->assertSame($ticket['source'], 'documents');
+        $this->assertSame($ticket['column_key'], 'number.ticket');
+
+        $invoice = $collect->last( )->toArray();
+
+        $this->assertSame($invoice['sequence'], 1);
+        $this->assertSame($invoice['source'], 'documents');
+        $this->assertSame($invoice['column_key'], 'number.invoice');
+
+    }
+
+}
