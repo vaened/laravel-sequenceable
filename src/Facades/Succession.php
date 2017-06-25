349be3d6c0c1c90094774333544e87e2a6bdd2f6
enea dhack <enea.so@live.com>
2017-06-25 18:06:51 -0500

    Add facade

---
+++ src/Facades/Succession.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by enea dhack - 24/06/17 01:58 PM
+ */
+
+namespace Enea\Sequenceable\Facades;
+
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Facade;
+use Enea\Sequenceable\Succession as BaseSuccession;
+
+
+/**
+ * Class Succession
+ * @package Enea\Sequenceable\Facade
+ * @author enea dhack <enea.so@live.com>
+ *
+ * Methods
+ *
+ * @method static Collection to( string|int $class )
+ *
+ * @see \Enea\Sequenceable\Succession
+ */
+class Succession extends Facade
+{
+
+    protected static function getFacadeAccessor()
+    {
+        return BaseSuccession::class;
+    }
+
+
+}
---
+++ src/Succession.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by enea dhack - 24/06/17 02:01 PM
+ */
+
+namespace Enea\Sequenceable;
+
+
+use Enea\Sequenceable\Contracts\SequenceableContract;
+use Enea\Sequenceable\Contracts\SequenceContract;
+use Enea\Sequenceable\Exceptions\SequenceException;
+use function foo\func;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+class Succession
+{
+    /**
+     * @var Builder
+     */
+    private $builder;
+
+    /**
+     * Succession constructor.
+     * @param Builder $builder
+     */
+    public function __construct( Builder $builder )
+    {
+        $this->builder = $builder;
+    }
+
+
+    /**
+     * @param Model|SequenceableContract $class
+     * @param array $keys
+     * @return Collection
+     * @throws SequenceException
+     */
+    public function to( $class, array $keys = array() ): Collection
+    {
+        if ( is_string( $class ) ) {
+            $class  = new $class;
+        }
+
+        if ( ! $class instanceof SequenceableContract ) {
+            throw new SequenceException('The model '. get_class( $class ) . ' must implement the '. SequenceableContract::class);
+        }
+
+        $this->builder->setSequenceableModel( $class );
+
+        $sequences = collect( );
+
+        $class->getSequenceModels( )->each( function ( $value, $key ) use ( $class, $sequences ) {
+            /** @var SequenceContract $sequence*/
+            $sequence = new $key;
+
+             $sequence->source($class->getTable( ))->each( function( SequenceContract $sequence ) use ( $sequences ) {
+                 $sequences->put($sequence->getColumnKey( ) , $sequence);
+            });
+        });
+
+        return $sequences;
+    }
+
+}
