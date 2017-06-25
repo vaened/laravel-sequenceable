f31a1228c8d6a86eccb1ed32f00c962d0576cde4
enea dhack <enea.so@live.com>
2017-06-24 23:58:18 -0500

    Separate logic in help file

---
+++ src/Helper.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by enea dhack - 24/06/17 09:56 PM
+ */
+namespace Enea\Sequenceable;
+
+class Helper
+{
+
+    /**
+     * Validates that the sequence is within the formats supported by the package
+     *
+     * @param array|string|int $key
+     * @param array|string|int $value
+     * @return bool
+     */
+    public static function isAvailableSequence( $key, $value ): bool
+    {
+        if( is_string( $key ) ) {
+            if ( is_array($value)) {
+                $key = key($value);
+                $value = current($value);
+
+                return is_integer($value) || is_integer($key) && is_string($value);
+            }
+
+            return is_string($value) || is_integer($value) && !is_numeric($key);
+        }
+
+        return is_integer($key) && is_string($value);
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
+    public static function getKeyName($key, $value )
+    {
+        return is_integer($key) ? $value : $key;
+    }
+
+    /**
+     * @param string|integer $key
+     * @param string|array $value
+     * @return string
+     */
+    public static function getColumnName($key, $value )
+    {
+        if (is_integer( $key )) {
+            return $value;
+        }
+
+        if ( is_array( $value )) {
+            $key = key( $value );
+            return is_string($key) ? $key : current($value);
+        }
+
+        return is_integer($value) ? $key : $value;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
+    public static function getSize($key, $value)
+    {
+
+        if( is_string( $value ) && ( is_integer( $key ) || is_string( $key ) )) {
+            return 0;
+        }
+
+        if (is_array($value)) {
+            $value = current($value);
+            return is_integer($value) ? $value : 0;
+        }
+
+        return $value;
+    }
+
+}
