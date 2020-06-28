c2946e8727babf20a4c37c70662a316132f12a33
enea <enea.so@live.com>
2020-06-28 14:19:20 -0500

    clean config file

--- config/sequenceable.php
+++ config/sequenceable.php
@@ -15,28 +15,5 @@ return [
     |
     */
 
-    'model' => \Enea\Sequenceable\Model\Sequence::class,
-
-    /*
-     |---------------------------------------------------------------------------
-     | Auto filling
-     |---------------------------------------------------------------------------
-     | This option allows you to automatically fill the fields in your database, as specified in the model configuration.
-     | If you want to customize the value with which to autofill, you must change this key by replacing the
-     | value with the character you want, by default, if it is true, it will autocomplete to zero
-     |
-     */
-
-    'autofilling' => false,
-
-    /*
-     |---------------------------------------------------------------------------
-     | Prefix
-     |---------------------------------------------------------------------------
-     | This key specifies the prefix that is used when you need to display the sequence with the number of characters
-     | defined in the configuration, if you do not want to use any prefix, set this key to null
-     |
-     */
-
-    'prefix' => 'full_'
+    'model' => \Enea\Sequenceable\Model\Sequence::class
 ];
